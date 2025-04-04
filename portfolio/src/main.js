f12ea53a44dcdb835cf611a0d621a227b1b4d00d
ajmal123-coder <ajmalsadhath@gmail.com>
2025-04-04 15:40:13 +0530

    update

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,9 +1,27 @@
 const lines =document.querySelector('#threeLine');
 const menu =document.querySelector('#menu');
+const l1 = document.getElementById('li1');
+const l2 = document.getElementById('li2');
+const l3 = document.getElementById('li3');
+const l4 = document.getElementById('li4');
 
 lines.addEventListener("click",()=>{
     menu.classList.toggle("hidden");
 })
+
+l1.addEventListener("click",() => {
+         menu.classList.toggle('hidden');
+})
+l2.addEventListener("click",() => {
+    menu.classList.toggle('hidden');
+})
+l3.addEventListener("click",() => {
+    menu.classList.toggle('hidden');
+})
+l4.addEventListener("click",() => {
+    menu.classList.toggle('hidden');
+})
+
 
 function sendMail(){
     var parmas = {
